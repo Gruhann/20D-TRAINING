d61a08857b7331f7e28e6ed3ffb3035db4edd51f
Gruhann <146216867+Gruhann@users.noreply.github.com>
2023-09-28 22:04:03 +0530

    Delete class_basics.py

--- class_basics.py
+++
@@ -1,8 +0,0 @@
-class Siva:
-    def exsiva(bbroy,a):
-        print("Hai babu",a)
-    def mrssiva(bbroy,b,c):
-        print("am on the way",b,c)
-child=Siva()
-child.exsiva(10000)
-child.mrssiva(20000,'take care')
