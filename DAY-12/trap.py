d4e312ebf09450624198c0465793b71e0fdac57f
Gruhann <146216867+Gruhann@users.noreply.github.com>
2023-10-11 09:25:17 +0530

    Add files via upload

---
+++ DAY-12/trap.py
@@ -0,0 +1,12 @@
+# def trap(height:):
+#     if not height:return True
+#     l=0
+#     r=len(height)-1
+#     maxl=height-[l]
+#     maxr=height-[r]
+#     res=0
+#     while l<r:
+#         if maxl<max:
+#             l+=1
+#             maxl=max(maxl,height[l])
+#             res+=max
